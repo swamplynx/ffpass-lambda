bc9fc5243fd21d6988502ea92e24aaca9c10d3ee
swamplynx <swamplynx@users.noreply.github.com>
2021-02-09 23:20:06 -0500

    Update index.js
    Added additional options

--- index.js
+++ index.js
@@ -28,8 +28,18 @@ exports.handler = async function(event, context, callback) {
         console.log(`Downloading vehicle status data...\n`)
         try {
             var vehicleData = await car.status()
-            console.log(`Engine State: ${vehicleData.ignitionStatus.value} \t\t(Refreshed: ${vehicleData.ignitionStatus.timestamp})`)
-                var engine = `${vehicleData.ignitionStatus.value}`
+            console.log(JSON.stringify(vehicleData))
+            console.log(`Ignition State: ${vehicleData.ignitionStatus.value} \t\t(Refreshed: ${vehicleData.ignitionStatus.timestamp})`)
+                var engine = `${vehicleData.ignitionStatus.value.toUpperCase()}`
+                var enginestate = `Your vehicle's ignition is currently ${vehicleData.ignitionStatus.value.toLowerCase()}.`
+            console.log(`Remote Start State: ${vehicleData.remoteStartStatus.value} \t\t(Refreshed: ${vehicleData.remoteStartStatus.timestamp})`)
+                if (vehicleData.remoteStartStatus.value == '1') {
+                var remotestart = 'ACTIVE' }
+                else { var remotestart = 'OFF' }
+                var rmstartstate = `Your vehicle's remote start is now ${remotestart.toLowerCase()}.`
+            console.log(`Door Locks: ${vehicleData.lockStatus.value} \t\t(Refreshed: ${vehicleData.lockStatus.timestamp})`)
+                var locks = `${vehicleData.lockStatus.value}`
+                var lockstate = `Your vehicle is now ${vehicleData.lockStatus.value.toLowerCase()}.`
             console.log(`Odometer Reading: ${Math.round(vehicleData.odometer.value/1.609344)} miles \t(Refreshed: ${vehicleData.odometer.timestamp})`)
                 var odometerread = `${Math.round(vehicleData.odometer.value/1.609344)}`
             console.log(`Battery Status: ${vehicleData.battery.batteryHealth.value} \t(Refreshed: ${vehicleData.battery.batteryHealth.timestamp})`)
@@ -43,10 +53,11 @@ exports.handler = async function(event, context, callback) {
                 var gasleft = `You have ${Math.round(vehicleData.fuel.distanceToEmpty/1.609344)} miles till empty.`
             console.log(`Vehicle Location: \n${JSON.stringify(vehicleData.gps)}`)
                 var gpsloc = `${JSON.stringify(vehicleData.gps)}`
+                var latlonloc = `${vehicleData.gps.latitude} ${vehicleData.gps.longitude}`
                 var location = await geocoder.reverse({lat:vehicleData.gps.latitude, lon:vehicleData.gps.longitude})
 
 let responseBody = {
-       engine, odometerread, dte, oillife, batterystat, tirepress, gpsloc, location, gasleft
+       engine, remotestart, locks, odometerread, dte, oillife, batterystat, tirepress, gpsloc, latlonloc, location, gasleft, enginestate, rmstartstate, lockstate
     };
           
           var response = {
@@ -61,7 +72,50 @@ let responseBody = {
                 "isBase64Encoded": false
                  };
         }
-    } else {
+    } 
+    
+    else if (event.headers.fast == '1') {
+        try {
+            var result = await car.issueCommand(event.headers.command)
+            console.log(`Issuing the ${event.headers.command} command fast. Result: ${code.getMessage(result.status)}`)
+            
+            if (await car.commandStatus(event.headers.command, result.commandId) == 552) {
+                 console.log(`Command: ${event.headers.command} sent successfully!`)
+                 let responseBody = {
+                    message : 'Command sent successfully!'
+                                    };
+                var response = {
+                    "statusCode": 200,
+                    body: JSON.stringify(responseBody),
+                    "isBase64Encoded": false
+                    };
+                    
+            } else {
+                console.log(`There was an error sending the command to the vehicle. Code: ${await car.commandStatus(event.headers.command, result.commandId)}`)
+                let responseBody = {
+                    message : 'Error sending the command!'
+                                    };
+                var response = {
+                     "statusCode": 411,
+                     body: JSON.stringify(responseBody),
+                    "isBase64Encoded": false
+                    };
+
+            }
+        } catch (error) {
+            console.log(`There was an error sending the command to the cloud: ${event.headers.command} to the vehicle!`)
+            let responseBody = {
+                    message : 'Error sending the command!'
+                                    };
+            var response = {
+                "statusCode": 500,
+                body: JSON.stringify(responseBody),
+                "isBase64Encoded": false
+                };
+        }
+    }
+    
+    else {
         try {
             var result = await car.issueCommand(event.headers.command)
             console.log(`Issuing the ${event.headers.command} command. Result: ${code.getMessage(result.status)}`)
@@ -76,7 +130,7 @@ let responseBody = {
                     message : 'Error executing the command!'
                                     };
                 var response = {
-                     "statusCode": 500,
+                     "statusCode": 411,
                      body: JSON.stringify(responseBody),
                     "isBase64Encoded": false
                     };
@@ -94,7 +148,7 @@ let responseBody = {
 
             }
         } catch (error) {
-            console.log(`There was an error sending the command: ${event.headers.command} to the vehicle!`)
+            console.log(`There was an error sending the command to the cloud: ${event.headers.command} to the vehicle!`)
             let responseBody = {
                     message : 'Error sending the command!'
                                     };
